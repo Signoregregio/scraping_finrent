a2ea9c46fdf0595871f9d79407a551143187ffb1
Luca <Luca>
2025-10-01 15:14:30 +0200

    ordine carsToShow

--- data.js
+++ data.js
@@ -19,25 +19,25 @@ function showCars(autoArray, indexCarsToShowRaw) {
   const container = document.getElementById("container-auto");
   container.innerHTML = "";
 
-  // Funzione per convertire indice negativo in reale
   function realIndex(i, length) {
     return i >= 0 ? i : length + i;
   }
 
-  // Calcola gli indici reali corretti da carsToShow
   const indexCarToShow = indexCarsToShowRaw.map((indexShow) =>
     realIndex(indexShow, autoArray.length)
   );
 
-  autoArray.forEach((auto, index) => {
-    // Mostra solo le auto il cui indice è presente in indicesToShow
-    if (indexCarToShow.includes(index)) {
-      const card = document.createElement("div");
-      card.className = "card-auto";
-      card.innerHTML = `
-        <img src="${
-          auto.imageUrls?.[0] || "placeholder.jpg"
+  indexCarToShow.forEach((index) => {
+    const auto = autoArray[index];
+    const card = document.createElement("div");
+    card.className = "card-auto";
+    card.innerHTML = `
+        <img class="immagine-auto" src="${
+          auto.imageUrls?.[0] || ""
         }" alt="Foto macchina">
+        <div class="etichetta-label">${auto.etichetta?.label || ""}</div>
+        <img class="etichetta-icon" src="${auto.etichetta?.icon || ""}" alt="">
+        <img class="timbro-icon" src="${auto.timbroUrl || ""}" alt="">
         <h3 class="nome-macchina">${auto.nomeMacchina}</h3>
         <p class="motore">Motore: ${auto.motore}</p>
         <p class="prezzo">Prezzo: ${auto.prezzo || "N.D."}</p>
@@ -45,7 +45,6 @@ function showCars(autoArray, indexCarsToShowRaw) {
         <p class="mesi">Durata: ${auto.mesi} mesi</p>
         <p class="anticipo">Anticipo: ${auto.anticipo || "Nessuno"}</p>
       `;
-      container.appendChild(card);
-    }
+    container.appendChild(card);
   });
 }
